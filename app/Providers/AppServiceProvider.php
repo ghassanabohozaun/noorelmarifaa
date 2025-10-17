0d146cf066dc63614a35101e21e8c6238db5aa3b
ghassan <ghassan@digitalorder.net>
2025-10-17 15:58:44 +0300

    child

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,13 +21,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
-         Paginator::useBootstrap();
+        Paginator::useBootstrap();
 
         foreach (config('global.permissions') as $ability => $value) {
             Gate::define($ability, function ($auth) use ($ability) {
                 return $auth->hasAbility($ability);
             });
         }
+
+        \Illuminate\Http\Request::macro('hasValidSignature', function ($absolute = true) {
+            if ('livewire/upload-file' || 'livewire/preview-file' == request()->path()) {
+                return true;
+            }
+            return \Illuminate\Support\Facades\URL::hasValidSignature($this, $absolute);
+        });
     }
 }
