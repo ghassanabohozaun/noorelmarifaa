6b3c53bc31b64f21de038123becf5632b6139005
ghassan <ghassan@digitalorder.net>
2025-11-25 10:51:44 +0200

    test

--- resources/views/frontend/photos-gallery-paging.blade.php
+++ resources/views/frontend/photos-gallery-paging.blade.php
@@ -1,4 +1,4 @@
-@if($photoAlbums->isEmpty())
+@if ($photoAlbums->isEmpty())
     <div class="row clearfix">
         <h3 class="text-warning">
             {!! trans('frontend.no_exists') !!}
@@ -8,8 +8,8 @@
     </div>
 @else
     <div class="row clearfix">
-    @foreach($photoAlbums as $photoAlbum)
-        <!-- Video Column -->
+        @foreach ($photoAlbums as $photoAlbum)
+            <!-- Video Column -->
             <div class="video-column col-lg-4 col-md-4 col-sm-4">
                 <div class="inner-column photos_gallery_box">
                     <!--Video Box-->
@@ -17,20 +17,18 @@
                     <a href="#" class="show_photo_gallery_modal" data-id="{!! $photoAlbum->id !!}">
                         <div class="video-box wow fadeInRight" data-wow-delay="0ms" data-wow-duration="1500ms">
                             <figure class="video-image">
-                                <img src="{!! asset(Storage::url($photoAlbum->main_photo)) !!}" alt=""
-                                     style="height: 300px">
+                                <img src="{!! asset(Storage::url($photoAlbum->main_photo)) !!}" alt="" style="height: 300px">
                             </figure>
 
                             <p>
                                 <a href="#" class="show_photo_gallery_modal" data-id="{!! $photoAlbum->id !!}">
-                                    {!! Lang()=='ar' ? $photoAlbum->title_ar : $photoAlbum->title_en  !!}
+                                    {!! Lang() == 'ar' ? $photoAlbum->title_ar : $photoAlbum->title_en !!}
                                 </a>
                             </p>
                         </div>
                     </a>
                 </div>
             </div>
-
         @endforeach
     </div>
 @endif
@@ -38,6 +36,6 @@
 <!--Post Share Options-->
 <div class="text-center">
     <ul class="clearfix">
-        {{ $photoAlbums->links('vendor.pagination.bootstrap-4') }}
+        {{ $photoAlbums->links() }}
     </ul>
 </div>
