9772eae34c0d47bbadcfa3c649635303864e603d
ghassan <ghassan@digitalorder.net>
2025-11-26 10:43:12 +0200

    photos

---
+++ lang/ar/communicationRequests.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    'send_message' => 'ارسال رسالة',
+    'send_message_successfully' => 'تم عملية ارسال الرسالة بنجاح',
+    'hodou_website' => 'موقع هدوء الطبي',
+    'contacted' => 'تم الاتصال',
+    'not_contacted' => 'لم يتم الاتصال',
+    'update_status' => 'تغير الحالة',
+    'all' => 'الكل',
+
+    'sender' => 'اسم المرسل',
+    'email' => 'البريد الالكتروني',
+    'title' => 'العنوان',
+    'details' => 'التفاصيل',
+    'status' => 'الحالة',
+
+    'enter_sender' => 'اسم المرسل',
+    'enter_email' => 'البريد الالكتروني',
+    'enter_title' => 'العنوان',
+    'enter_details' => 'التفاصيل',
+    'enter_status' => 'الحالة',
+
+    'required'=>'هذا الحقل مطلوب',
+    'sender_required' => 'اسم المرسل مطلوب',
+    'email_required' => ' البريد الالكتروني مطلوب',
+    'email_email' => 'يجب ان تكون صيغة الايميل مثل admin@admin.com',
+    'title_required' => 'عنوان الرسالة مطلوب',
+    'details_required' => 'تفاصيل الرسالة مطلوبة',
+
+];
---
+++ lang/en/communicationRequests.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    'send_message'=>'Send Message',
+    'send_message_successfully'=>'Send Message Successfully',
+    'contacted'=>'Contacted',
+    'not_contacted'=>'Not Contacted',
+    'update_status'=>'Update Status',
+    'all'=>'all',
+
+    'sender'=>'Sender',
+    'email'=>'Email',
+    'title'=>'Title',
+    'details'=>'Details',
+    'status'=>'Status',
+
+    'enter_sender'=>'Enter Sender',
+    'enter_email'=>'Enter Email',
+    'enter_title'=>'Enter Title',
+    'enter_details'=>'Enter Details',
+    'enter_status'=>'Enter Status',
+
+    'required' => 'This Field is Required ',
+    'sender_required'=>'Sender Required',
+    'email_required'=>'Email Required',
+    'email_email'=>'Email Must be in this format admin@admin.com',
+    'title_required'=>'Title Required',
+    'details_required'=>'Details Required',
+    'status_required'=>'Status Required',
+
+];
