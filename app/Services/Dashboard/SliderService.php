b62b55172090162ee830c659c5df055cd133c3a1
ghassan <ghassan@digitalorder.net>
2025-12-08 09:54:19 +0200

    slider

--- app/Services/Dashboard/SliderService.php
+++ app/Services/Dashboard/SliderService.php
@@ -68,7 +68,7 @@ class SliderService
     public function create($data)
     {
         if (array_key_exists('photo', $data) && $data['photo'] != null) {
-            $photo_name = $this->imageManagerUtils->saveResizeImage($data['photo'], 'sliders', 1700, 1000);
+            $photo_name = $this->imageManagerUtils->saveResizeImage($data['photo'], 'sliders', 1920, 742);
             $data['photo'] = $photo_name;
         }
 
@@ -86,7 +86,7 @@ class SliderService
 
         if (array_key_exists('photo', $data) && $data['photo'] != null) {
             $this->imageManagerUtils->removeImageFromLocal($slider->photo, 'sliders');
-            $photo_name = $this->imageManagerUtils->saveResizeImage($data['photo'], 'sliders', 1700, 1000);
+            $photo_name = $this->imageManagerUtils->saveResizeImage($data['photo'], 'sliders', 1920, 742);
             $data['photo'] = $photo_name;
         }
 
