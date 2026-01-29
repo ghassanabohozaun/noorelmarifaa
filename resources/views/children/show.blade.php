a423c8e4c9e9f5cc9257f4794d43b78c1458c132
ghassan <ghassan@digitalorder.net>
2026-01-29 12:37:03 +0200

    test

--- resources/views/children/show.blade.php
+++ resources/views/children/show.blade.php
@@ -26,7 +26,7 @@
 
                                 <div class="col-auto">
                                     <div class="mb-2">
-                                        @if ($child->childFile === null)
+                                        @if (!$child->childFile()->exists())
                                             <img src="{!! asset('uploads/children/' . $child->childFile->picture_of_the_orphan_child) !!}" alt="profile_image"
                                                 class="border-radius-lg shadow-sm rounded" style="width: 150px">
                                         @endif
